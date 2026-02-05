69b80ae1ba8ddf5c0159c6aa743344c20b7a7d1f
Emre Cihangir <emrecihangir@192.168.0.112>
2026-02-05 18:04:24 +0300

    Fix: Import Sequelize 'Op' operator to resolve 500 error

--- server/routes/salary.js
+++ server/routes/salary.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { SalaryPayment, Employee, Attendance } = require('../models');
+const { Op } = require('sequelize');
+const { Employee, SalaryPayment, Attendance } = require('../models');
 const { requireAuth } = require('../middleware/auth');
 const { Op } = require('sequelize');
 const sequelize = require('sequelize');
