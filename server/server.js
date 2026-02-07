8fc9bb53661e55b9883fab4f0d8fc6478468072a
Emre Cihangir <emrecihangir@192.168.0.104>
2026-02-07 23:14:37 +0300

    UI Fixes: Remove hint, update Stok Sorumlusu permissions

--- server/server.js
+++ server/server.js
@@ -102,7 +102,7 @@ const syncRolesAndPermissions = async () => {
             // System Roles
             { name: 'Yönetici', permissions: ['all'], is_system: true },
             { name: 'Personel', permissions: ['view_jobs', 'view_products', 'view_attendance'], is_system: true },
-            { name: 'Stok Sorumlusu', permissions: ['view_sources', 'manage_stock'], is_system: false }
+            { name: 'Stok Sorumlusu', permissions: ['view_sources', 'view_products', 'manage_stock'], is_system: false }
         ];
 
         // 1.1 Delete Legacy/Unused Roles
